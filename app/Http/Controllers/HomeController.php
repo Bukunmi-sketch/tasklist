5a4b6017290fb880d900aaf1da03685e37c6c24b
Bukunmi Olarinde <58373089+Bukunmi-sketch@users.noreply.github.com>
2022-07-17 22:57:15 -0700

    Update HomeController.php
    ---

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class HomeController extends Controller
 {
     /**
@@ -17,7 +18,7 @@ class HomeController extends Controller
     }
 
     /**
-     * Show the application dashboard.
+     * Show the application dashboard
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
